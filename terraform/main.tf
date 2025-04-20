628d75e5bd70e5583f873d877ac53e444824e2b4
Paul Benetis <paulbenetis@gmail.com>
2025-04-20 14:31:30 +0200

    Store the state remotely in s3 (#2)

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,23 @@ terraform {
       version = "5.95.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "paulb-devops-toolkit-terraform-state"
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
   shared_config_files      = ["/Users/paulbenetis/.aws/config"]
   shared_credentials_files = ["/Users/paulbenetis/.aws/credentials"]
   profile                  = "default"
+
+  default_tags {
+    tags = {
+      Project = "devops-toolkit"
+    }
+  }
 }
