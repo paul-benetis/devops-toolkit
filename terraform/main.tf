c4bc69326bec6473b8a65e53bdb34404d0c4d9b7
paul-benetis <paulbenetis@gmail.com>
2025-05-19 20:18:28 +0200

    fix gh provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.95.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
   }
 
   backend "s3" {
