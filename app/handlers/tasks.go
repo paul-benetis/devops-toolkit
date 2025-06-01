7d6253f5d6744cbc9b9a670e3a405befd8381709
paul-benetis <paulbenetis@gmail.com>
2025-06-01 19:55:52 +0200

    Small app change to test deploy

--- app/handlers/tasks.go
+++ app/handlers/tasks.go
@@ -11,7 +11,7 @@ func TasksHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tasks := []string{"Task 1", "Task 2", "Task 3"}
+	tasks := []string{"Task 1", "Task 2", "Task 3", "Task 4", "Task 5"}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
